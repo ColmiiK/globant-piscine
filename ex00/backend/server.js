82072a08e9f3b1d0e27b108f6ab900d13646a282
Álvaro Vega Gómez <alvarvg@gmail.com>
2024-10-22 16:19:33 +0200

    cleanup a bit

--- ex00/backend/server.js
+++ ex00/backend/server.js
@@ -1,23 +1,25 @@
 const cors = require("cors");
 const express = require("express");
 const axios = require("axios");
-require("dotenv").config();
-
 const app = express();
 const port = 5000;
+require("dotenv").config();
+const UNSPLASH_API_KEY = process.env.UNSPLASH_API_KEY;
+
 app.use(
   cors({
     origin: "http://localhost:8080", // Allow frontend on localhost:8080
     methods: ["GET", "POST"],
   }),
 );
-const UNSPLASH_API_KEY = process.env.UNSPLASH_API_KEY;
-app.get("/api/random-image", async (req, res) => {
+
+// Consume the Unsplash API to get photos
+app.get("/api/image", async (req, res) => {
   const query = req.query.query;
   const imageCount = 4;
-
   try {
     let response;
+    let images;
     if (query) {
       response = await axios.get("https://api.unsplash.com/search/photos", {
         params: {
@@ -26,13 +28,8 @@ app.get("/api/random-image", async (req, res) => {
           per_page: imageCount,
         },
       });
-      const images = response.data.results;
-      if (images && images.length > 0) {
-        const imageUrls = images.map((image) => image.urls.regular);
-        res.json({ imageUrls });
-      } else {
-        res.status(500).json({ error: "No images found" });
-      }
+
+      images = response.data.results;
     } else {
       response = await axios.get("https://api.unsplash.com/photos/random", {
         params: {
@@ -40,56 +37,17 @@ app.get("/api/random-image", async (req, res) => {
           client_id: UNSPLASH_API_KEY,
         },
       });
-      const images = response.data;
-      if (images && images.length > 0) {
-        const imageUrls = images.map((image) => image.urls.regular);
-        res.json({ imageUrls });
-      } else {
-        res.status(500).json({ error: "No random images found" });
-      }
+      images = response.data;
+    }
+    if (images && images.length > 0) {
+      const imageUrls = images.map((image) => image.urls.regular);
+      res.json({ imageUrls });
+    } else {
+      res.status(500).json({ error: "No images found" });
     }
   } catch (error) {
     console.error("Error fetching image:", error);
     res.status(500).json({ error: "Failed to fetch image from Unsplash" });
-  }
-});
-
-async function exchangeCodeForToken(code) {
-  const UNSPLASH_API_KEY = process.env.UNSPLASH_API_KEY;
-  const UNSPLASH_SECRET = process.env.UNSPLASH_SECRET;
-  const REDIRECT_URI = "http://localhost:5000/auth/callback";
-  try {
-    const response = await axios.post(
-      "https://unsplash.com/oauth/token",
-      null,
-      {
-        params: {
-          client_id: UNSPLASH_API_KEY,
-          client_secret: UNSPLASH_SECRET,
-          redirect_uri: REDIRECT_URI,
-          code: code,
-          grant_type: "authorization_code",
-        },
-      },
-    );
-    const accessToken = response.data.access_token;
-    return accessToken;
-  } catch (error) {
-    console.error(
-      "Error exchanging code for access token:",
-      error.response.data,
-    );
-    throw new Error("Failed to obtain access token");
-  }
-}
-
-app.get("/auth/callback", async (req, res) => {
-  const { code } = req.query;
-  try {
-    const accessToken = await exchangeCodeForToken(code);
-    res.send("Authentification successful! Feel free to close this window");
-  } catch (error) {
-    res.status(500).send("Error exchanging code for access token");
   }
 });
 
--- ex00/src/index.ts
+++ ex00/src/index.ts
@@ -5,56 +5,46 @@ const loadButton = document.getElementById("load-button") as HTMLButtonElement;
 const queryInput = document.getElementById("query") as HTMLInputElement;
 const authInput = document.getElementById("auth") as HTMLInputElement;
 
+// Adds the images to the DOM
+function addImages(imageUrls: string[]) {
+  imageContainer.innerHTML = "";
+  imageUrls.forEach((url) => {
+    const imageElement = document.createElement("img");
+    imageElement.src = url;
+    imageContainer.appendChild(imageElement);
+  });
+}
+
+// Parses response as json
+async function handleResponse(response: any) {
+  const data = await response.json();
+  console.log("API response:", data);
+  const imageUrls: string[] = data.imageUrls;
+  addImages(imageUrls);
+}
+
+// Load images from a query
 async function loadImage(): Promise<void> {
   const query = queryInput.value;
   try {
     const response = await fetch(
-      `http://localhost:5000/api/random-image?query=${encodeURIComponent(query)}`,
+      `http://localhost:5000/api/image?query=${encodeURIComponent(query)}`,
     );
-    const data = await response.json();
-    console.log("API response:", data);
-    const imageUrls: string[] = data.imageUrls;
-
-    imageContainer.innerHTML = "";
-
-    imageUrls.forEach((url) => {
-      const imageElement = document.createElement("img");
-      imageElement.src = url;
-      imageContainer.appendChild(imageElement);
-    });
+    await handleResponse(response);
   } catch (error) {
     console.error("Error fetching image:", error);
   }
 }
 
+// Load random images with no query
 async function loadImageRandom(): Promise<void> {
   try {
-    const response = await fetch(`http://localhost:5000/api/random-image`);
-    const data = await response.json();
-    console.log("API response:", data);
-    const imageUrls: string[] = data.imageUrls;
-    imageContainer.innerHTML = "";
-
-    imageUrls.forEach((url) => {
-      const imageElement = document.createElement("img");
-      imageElement.src = url;
-      imageContainer.appendChild(imageElement);
-    });
+    const response = await fetch(`http://localhost:5000/api/image`);
+    await handleResponse(response);
   } catch (error) {
     console.error("Error fetching image:", error);
   }
 }
-
-function redirectToOAuth() {
-  const clientId = authInput.value;
-  const redirectUri = "http://localhost:5000/auth/callback";
-  const scope = "public";
-
-  const authUrl = `https://unsplash.com/oauth/authorize?client_id=${encodeURIComponent(clientId)}&redirect_uri=${encodeURIComponent(redirectUri)}&response_type=code&scope=${encodeURIComponent(scope)}`;
-  window.location.href = authUrl;
-}
-
-window.onload = loadImageRandom;
 
 loadButton.addEventListener("click", loadImageRandom);
 queryInput.addEventListener("keydown", (event: KeyboardEvent) => {
@@ -64,6 +54,7 @@ queryInput.addEventListener("keydown", (event: KeyboardEvent) => {
 });
 authInput.addEventListener("keydown", (event: KeyboardEvent) => {
   if (event.key === "Enter") {
-    redirectToOAuth();
   }
 });
+
+window.onload = loadImageRandom;
